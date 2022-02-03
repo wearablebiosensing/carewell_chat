001da264591c8f592d98118c0aae7c5b98da4b96
tylerhanlon <54967903+tylerhanlon@users.noreply.github.com>
2022-02-02 20:23:18 -0500

    Username is being saved in database

--- lib/views/signup.dart
+++ lib/views/signup.dart
@@ -1,6 +1,10 @@
 import 'package:chat_application/views/signin.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_application/widgets/widget.dart';
+import 'package:chat_application/services/auth_services.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -25,117 +29,125 @@ class _SignUpState extends State<SignUp> {
         appBar: appBarMain(context),
         body: Container(
           padding: EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            children: [
-              //Make these all have controller: properties
-              TextField(
-                controller: emailTextEditingController,
-                decoration: InputDecoration(
-                    hintText: "Email",
-                    hintStyle: TextStyle(color: Colors.grey[500])),
-              ),
-
-              TextField(
-                controller: passwordTextEditingController,
-                decoration: InputDecoration(
-                    hintText: "Password",
-                    hintStyle: TextStyle(color: Colors.grey[500])),
-              ),
-              //textField("email"),
-              //textField("password"),
-
-              SizedBox(
-                height: 8,
-              ),
-              Container(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  child: Text(
-                    "Forgot Password?",
-                    style: new TextStyle(color: Colors.grey[700], fontSize: 12),
-                  ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                //Make these all have controller: properties
+                TextField(
+                  controller: emailTextEditingController,
+                  decoration: InputDecoration(
+                      hintText: "Email",
+                      hintStyle: TextStyle(color: Colors.grey[500])),
                 ),
-              ),
-              SizedBox(
-                height: 8,
-              ),
 
-              GestureDetector(
-                onTap: () {
-                  final String email = emailTextEditingController.text.trim();
-                  final String password =
-                      passwordTextEditingController.text.trim();
+                TextField(
+                  controller: passwordTextEditingController,
+                  decoration: InputDecoration(
+                      hintText: "Password",
+                      hintStyle: TextStyle(color: Colors.grey[500])),
+                ),
+                //textField("email"),
+                //textField("password"),
 
-                  if (email.isEmpty) {
-                    print("Email is empty");
-                  } else {
-                    if (password.isEmpty) {
-                      print("Password is empty");
-                    }
-                  }
-                },
-                child: Container(
-                  alignment: Alignment.center,
-                  width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.symmetric(vertical: 20),
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          //blue color background
-                          colors: [Color(0xff007EF4), Color(0xff2A75BC)]),
-                      borderRadius: BorderRadius.circular(30)),
-                  child: Text("Sign Up",
+                SizedBox(
+                  height: 8,
+                ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: Text(
+                      "Forgot Password?",
                       style:
-                          new TextStyle(color: Colors.white70, fontSize: 17)),
+                          new TextStyle(color: Colors.grey[700], fontSize: 12),
+                    ),
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 16,
-              ),
+                SizedBox(
+                  height: 8,
+                ),
 
-              GestureDetector(
-                onTap: () {
-                  final String email = emailTextEditingController.text.trim();
-                  final String password =
-                      passwordTextEditingController.text.trim();
+                GestureDetector(
+                  onTap: () {
+                    final String email = emailTextEditingController.text.trim();
+                    final String password =
+                        passwordTextEditingController.text.trim();
 
-                  if (email.isEmpty) {
-                    print("Email is empty");
-                  } else {
-                    if (password.isEmpty) {
-                      print("Password is empty");
+                    if (email.isEmpty) {
+                      print("Email is empty");
+                    } else {
+                      if (password.isEmpty) {
+                        print("Password is empty");
+                      } else {
+                        FirebaseFirestore.instance
+                            .collection('Users')
+                            .add({'username': email});
+                      }
                     }
-                  }
-                },
-                child: Container(
-                  alignment: Alignment.center,
-                  width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.symmetric(vertical: 20),
-                  decoration: BoxDecoration(
-                      color: Colors.white70,
-                      borderRadius: BorderRadius.circular(30)),
-                  child: Text("Sign Up with Google",
-                      style:
-                          new TextStyle(color: Colors.black87, fontSize: 17)),
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    decoration: BoxDecoration(
+                        // ignore: prefer_const_constructors
+                        gradient: LinearGradient(
+                            //blue color background
+                            colors: [Color(0xff007EF4), Color(0xff2A75BC)]),
+                        borderRadius: BorderRadius.circular(30)),
+                    child: Text("Sign Up",
+                        style:
+                            new TextStyle(color: Colors.white70, fontSize: 17)),
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Already have an account? ",
-                    style: TextStyle(color: Colors.black87, fontSize: 14),
+                SizedBox(
+                  height: 16,
+                ),
+
+                GestureDetector(
+                  onTap: () {
+                    final String email = emailTextEditingController.text.trim();
+                    final String password =
+                        passwordTextEditingController.text.trim();
+
+                    if (email.isEmpty) {
+                      print("Email is empty");
+                    } else {
+                      if (password.isEmpty) {
+                        print("Password is empty");
+                      }
+                    }
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    decoration: BoxDecoration(
+                        color: Colors.white70,
+                        borderRadius: BorderRadius.circular(30)),
+                    child: Text("Sign Up with Google",
+                        style:
+                            new TextStyle(color: Colors.black87, fontSize: 17)),
                   ),
-                  Text(
-                    "Sign in now",
-                    style: TextStyle(decoration: TextDecoration.underline),
-                  ),
-                ],
-              )
-            ],
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Already have an account? ",
+                      style: TextStyle(color: Colors.black87, fontSize: 14),
+                    ),
+                    Text(
+                      "Sign in now",
+                      style: TextStyle(decoration: TextDecoration.underline),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ));
   }
